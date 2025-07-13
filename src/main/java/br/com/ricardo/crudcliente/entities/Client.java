ccdfbc4f1300cd74a3387ab0c9226d6eb14d333e
Ricardo Rodrigues Santana <140956804+Ric-ardo28@users.noreply.github.com>
2025-07-12 22:57:24 -0300

    ajustes finais e padronização do projeto exceções

--- src/main/java/br/com/ricardo/crudcliente/entities/Client.java
+++ src/main/java/br/com/ricardo/crudcliente/entities/Client.java
@@ -1,6 +1,6 @@
 package br.com.ricardo.crudcliente.entities;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
